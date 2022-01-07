cc39982f48e33e2b99b40430346247a960989524
sudoku-lord <aditi@lob.com>
2022-01-06 16:53:10 -0800

    edited autocompletions file

--- __tests__/UsAutocompletionsApi.spec.ts
+++ __tests__/UsAutocompletionsApi.spec.ts
@@ -1,37 +1,40 @@
 import { Configuration } from "../configuration";
 
 import { UsAutocompletionsWritable } from "../models";
-import { UsAutocompletions } from "../api";
+import { USAutocompletionsApi } from "../api";
 
-describe("UsVerificationApi", () => {
+describe("USAutocompletionsApi", () => {
   const config: Configuration = new Configuration({
-    username: process.env.LOB_API_KEY,
+    username: process.env.LOB_LIVE_API_KEY,
   });
 
-  const autocomplete: UsAutocompletionsWritable = {
-    primary_line: "180 BERRY ST",
-    city: "SAN FRANCISCO",
+  const autocompletionInput: UsAutocompletionsWritable = {
+    address_prefix: "185 B",
+    city: "San Francisco",
     state: "CA",
     zip_code: "94107",
+    geo_ip_sort: false
   };
 
   it("US Autocompletions API can be instantiated", () => {
-    const autocompletionApi = new UsAutocompletions(config);
+    const autocompletionApi = new USAutocompletionsApi(config);
     expect(autocompletionApi).toBeDefined();
     expect(typeof autocompletionApi).toEqual("object");
-    expect(autocompletionApi).toBeInstanceOf(UsAutocompletions);
+    expect(autocompletionApi).toBeInstanceOf(USAutocompletionsApi);
   });
 
   describe("autocomplete", () => {
     it("exists", () => {
-      const autocompletionApi = new UsAutocompletions(config);
+      const autocompletionApi = new USAutocompletionsApi(config);
       expect(autocompletionApi.autocomplete).toBeDefined();
       expect(typeof autocompletionApi.autocomplete).toEqual("function");
     });
 
-    it("verifies a single US address", async () => {
-      const autocompletionApi = new UsAutocompletions(config);
-      const response = await autocompletionApi.autocomplete(autocomplete);
+    it("autocompletes given input", async () => {
+      const autocompletionApi = new USAutocompletionsApi(config);
+      const response = await autocompletionApi.autocomplete(autocompletionInput);
+      expect(response.suggestions).toBeDefined();
+      expect(response?.suggestions?.length).toBeGreaterThan(0);
     });
   });
 });
