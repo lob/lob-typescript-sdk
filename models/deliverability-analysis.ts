47265917f3f9f7d6c64c97f2251f8e2307360bef
sudoku-lord <aditi@lob.com>
2022-01-11 11:32:33 -0800

    fixed minor issue with deliverability analysis

--- models/deliverability-analysis.ts
+++ models/deliverability-analysis.ts
@@ -40,7 +40,7 @@ export class DeliverabilityAnalysis {
      */
     'dpv_vacant'?: DeliverabilityAnalysisDpvVacantEnum;
     /**
-     * Corresponds to the USPS field `dpv_no_stat`. indicates that an address has been vacated in the recent past, and is no longer receiving deliveries. If it\'s been unoccupied for 90+ days, or temporarily vacant, this will be flagged. Possible values are: * `Y` –– Address is active. * `N` –– Address is not active. * `\'\'` –– A DPV match is not made (`deliverability_analysis[dpv_confirmation]` is `N` or an empty string). 
+     * Corresponds to the USPS field `dpv_no_stat`. Indicates that an address has been vacated in the recent past, and is no longer receiving deliveries. If it\'s been unoccupied for 90+ days, or temporarily vacant, this will be flagged. Possible values are: * `Y` –– Address is active. * `N` –– Address is not active. * `\'\'` –– A DPV match is not made (`deliverability_analysis[dpv_confirmation]` is `N` or an empty string). 
      * @type {string}
      * @memberof DeliverabilityAnalysis
      */
