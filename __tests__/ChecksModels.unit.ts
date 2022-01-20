504452651e9fff62c1f2f39b0874d7df6dc3310a
BennyKitchell <bennykitchell@gmail.com>
2022-01-19 18:41:22 -0600

    fix: Fix models test

--- __tests__/ChecksModels.unit.ts
+++ __tests__/ChecksModels.unit.ts
@@ -1,72 +1,7 @@
-import { CheckAllOf } from "../models/check-all-of";
-import { CheckBaseAllOf, CheckBaseAllOfMailTypeEnum } from "../models/check-base-all-of"
 import { CheckDeletion } from "../models/check-deletion";
-import { CheckEditablePropsAllOf } from "../models/check-editable-props-all-of";
-import { CheckInputTo } from "../models/check-input-to";
+import { CheckEditableProps } from "../models/check-editable-props";
 
 describe("Check Models", () => {
-  describe("CheckAllOf", () => {
-    it("can be created", () => {
-      const rec = new CheckAllOf();
-      expect(rec).toBeDefined();
-    });
-
-    it("rejects invalid values for id", () => {
-      const rec = new CheckAllOf();
-      expect(rec.id).not.toBeDefined();
-
-      const invalidValues = ["Nope"];
-      for (const val of invalidValues) {
-        try {
-          rec.id = val;
-          throw new Error("Should Throw");
-        } catch (err: any) {
-          expect(err.message).toEqual("Invalid id provided");
-        }
-      }
-    });
-
-    it("allows setting valid values for id", () => {
-      const rec = new CheckAllOf();
-      expect(rec.id).not.toBeDefined();
-
-      const validValues = ["chk_1234"];
-      for (const val of validValues) {
-        rec.id = val;
-        expect(rec.id).toBeDefined();
-        expect(rec.id).toEqual(val);
-      }
-    });
-  });
-
-  describe("CheckBaseAllOf", () => {
-    it("can be created", () => {
-      const rec = new CheckBaseAllOf();
-      expect(rec).toBeDefined();
-    });
-
-
-    it("allows setting valid values for mail type", () => {
-      const rec = new CheckBaseAllOf();
-      expect(rec.mail_type).not.toBeDefined();
-
-      rec.mail_type = CheckBaseAllOfMailTypeEnum.UspsFirstClass;
-      expect(rec.mail_type).toBeDefined();
-    });
-
-    it("allows setting valid values for memo", () => {
-      const rec = new CheckBaseAllOf();
-      expect(rec.memo).not.toBeDefined();
-
-      const validValues = ["chk_1234"];
-      for (const val of validValues) {
-        rec.memo = val;
-        expect(rec.memo).toBeDefined();
-        expect(rec.memo).toEqual(val);
-      }
-    });
-  });
-
   describe("CheckDeletion", () => {
     it("can be created", () => {
       const rec = new CheckDeletion();
@@ -103,13 +38,13 @@ describe("Check Models", () => {
 
   describe("CheckEditablePropsAllOf", () => {
     it("can be created", () => {
-      const rec = new CheckEditablePropsAllOf();
+      const rec = new CheckEditableProps();
       expect(rec).toBeDefined();
     });
 
 
     it("allows setting valid values for mail type", () => {
-      const rec = new CheckEditablePropsAllOf();
+      const rec = new CheckEditableProps();
       expect(rec.amount).not.toBeDefined();
 
       rec.amount = 1;
@@ -117,7 +52,7 @@ describe("Check Models", () => {
     });
 
     it("allows setting valid values for memo", () => {
-      const rec = new CheckEditablePropsAllOf();
+      const rec = new CheckEditableProps();
       expect(rec.from).not.toBeDefined();
 
       const validValues = ["chk_1234"];
@@ -125,25 +60,6 @@ describe("Check Models", () => {
         rec.from = val;
         expect(rec.from).toBeDefined();
         expect(rec.from).toEqual(val);
-      }
-    });
-  });
-
-  describe("CheckInputTo", () => {
-    it("can be created", () => {
-      const rec = new CheckInputTo();
-      expect(rec).toBeDefined();
-    });
-
-    it("allows setting valid values for memo", () => {
-      const rec = new CheckInputTo();
-      expect(rec.to).not.toBeDefined();
-
-      const validValues = ["chk_1234"];
-      for (const val of validValues) {
-        rec.to = val;
-        expect(rec.to).toBeDefined();
-        expect(rec.to).toEqual(val);
       }
     });
   });
