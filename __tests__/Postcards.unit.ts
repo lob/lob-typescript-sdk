158a7bc1734c15d5ebe9f0258820b59d5706bc24
BennyKitchell <bennykitchell@gmail.com>
2022-07-01 11:58:54 -0500

    fix: tests broken by spec change

--- __tests__/Postcards.unit.ts
+++ __tests__/Postcards.unit.ts
@@ -331,7 +331,7 @@ describe("PostcardsApi", () => {
 
     it("lists postcards with a size parameter", async () => {
       axiosRequest.mockImplementationOnce(async (request) => {
-        expect(request.url.split("?")[1]).toEqual("size=4x6");
+        expect(request.url.split("?")[1]).toEqual(["size=4x6"]);
         return {
           data: { data: [{ id: "fake 1" }, { id: "fake 2" }] },
         };
